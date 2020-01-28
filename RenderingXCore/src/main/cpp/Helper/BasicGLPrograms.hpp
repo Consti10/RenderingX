6df2fcf9c21d855495e11e4f73554ff276a90c6c
Consti10 <geierconstantinabc@gmail.com>
2020-01-29 00:24:45 +0100

    use * for DistortionManager to also indicate disabled

--- RenderingXCore/src/main/cpp/Helper/BasicGLPrograms.hpp
+++ RenderingXCore/src/main/cpp/Helper/BasicGLPrograms.hpp
@@ -15,7 +15,7 @@
 
 class BasicGLPrograms{
 public:
-    explicit BasicGLPrograms(const DistortionManager* distortionManager):
+    explicit BasicGLPrograms(const DistortionManager* distortionManager= nullptr):
         vc(distortionManager),
         text(distortionManager),
         line(distortionManager),
