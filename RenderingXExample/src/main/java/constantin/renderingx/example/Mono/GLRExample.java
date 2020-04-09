f582c78f7be4e96b3e58afb70fd23e593bfc3480
Consti10 <geierconstantinabc@gmail.com>
2020-04-09 11:15:43 +0200

    use NDKHelper for VrHeadsetParams, rename some .cpp classes, split distortion mesh test & 360 video

--- RenderingXExample/src/main/java/constantin/renderingx/example/Mono/GLRExample.java
+++ RenderingXExample/src/main/java/constantin/renderingx/example/Mono/GLRExample.java
@@ -12,7 +12,7 @@ import javax.microedition.khronos.opengles.GL10;
 //See native code for documentation
 public class GLRExample implements GLSurfaceView.Renderer, MultiTouchGestureDetector.OnMultiTouchGestureListener{
     static {
-        System.loadLibrary("example-renderer");
+        System.loadLibrary("example-mono");
     }
     private native void nativeOnSurfaceCreated(final Context context);
     private native void nativeOnSurfaceChanged(int width,int height);
